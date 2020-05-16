a4ba27318997cf27afbd392e7801965953f019a3
wyatt8740 <wyatt8740@users.noreply.github.com>
2020-05-16 07:14:29 -0400

    added a script for generating rudimentary directory indices, also added an index

---
+++ generate_dir_index.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env sh
+STARTDIR="$PWD"
+
+htmlHead()
+{
+cat << EOF
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Index of 
+EOF
+echo "$1"
+cat << EOF
+</title>
+</head>
+<body>
+EOF
+}
+
+htmlFoot()
+{
+  echo '</body>'
+}
+
+buildPage()
+{
+  htmlHead "$1"
+  cd "$1"
+  find . -maxdepth 1 -type f -print0 | xargs -0 -i sh -c 'echo "<a href="''\"'{}'\"''">"{}"</a><br/>"'
+  htmlFoot
+  cd "$STARTDIR"
+}
+
+while [ "$#" -gt 0 ]; do
+  if [ -d "$1" ]; then
+    if [ -f "$1"'/index.html' ]; then
+      1>&2 echo 'E: Please delete the index.html in the directory '"$1"' if you really want to replace it.'
+      exit 1
+    fi
+    buildPage "$1" > "$STARTDIR"'/'"$(basename "$1")"'.html'
+    mv "$STARTDIR"'/'"$(basename "$1")"'.html' "$1"'/index.html'
+  fi
+  shift
+done
