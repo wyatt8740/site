8f6eaebacde3a28c93e421aba12562c4d2bc51b4
wyatt8740 <wyattfward@gmail.com>
2022-04-26 00:38:08 -0400

    dark site generator, but done better this time with symlinks

--- darkgen.sh
+++ darkgen.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 mkdir -p dark
-find blog/ about/ ./index.html -type f -iname '*.html' | while read -r line; do
-  mkdir -p 'dark/'"$(dirname "$line")"
-  sed 's/style\.css/style_dark.css/g' < "$line" > 'dark/'"$line"
-done
+ln -sf ../blog/ dark/blog
+ln -sf ../about/ dark/about
+ln -sf ../index.html dark/index.html
+ln -sf ../style_dark.css dark/style.css
+ln -sf blog dark/blerg
